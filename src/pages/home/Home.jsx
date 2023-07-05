58ae59b0d75ce509b615e47468445109e43da2e7
anwarkvzk <anvarvzk521@gmail.com>
2023-07-05 23:24:49 +0530

    Product Slider View

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,9 @@ import "./Home.scss";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/Slide/Slide";
-import { cards } from "../../data";
+import { cards, projects } from "../../data";
 import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 
 function Home() {
   return (
@@ -93,6 +94,11 @@ function Home() {
           </div>
         </div>
       </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} item={card} />
+        ))}
+      </Slide>
     </div>
   );
 }
