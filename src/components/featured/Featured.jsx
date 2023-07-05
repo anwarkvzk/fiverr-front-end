99fed677f27a60abde7b5b481151e75c5737dffc
anwarkvzk <anvarvzk521@gmail.com>
2023-07-05 10:20:16 +0530

    Featured Component Created

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Featured.scss";
+
+function Featured() {
+  return (
+    <div className="featured">
+      <div className="container">
+        <div className="left">
+          <h1>
+            Find the perfect <i>freelance</i> services for your business
+          </h1>
+          <div className="search">
+            <div className="searchInput">
+              <img src="./img/search.png" alt="" />
+              <input type="text" placeholder='Try "building mobil app"' />
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="popular">
+            <span>Popular:</span>
+            <button>Web Design</button>
+            <button>Wordpress</button>
+            <button>Logo Design</button>
+            <button>AI Services</button>
+          </div>
+        </div>
+        <div className="right"></div>
+        <img src="./img/me.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Featured;
