84f8219d1b9991e20c9c62da4e17dec9efd6dfa2
anwarkvzk <anvarvzk521@gmail.com>
2023-07-05 22:57:50 +0530

    Feature Component Design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import "./Home.scss"
-import Featured from '../../components/featured/Featured'
-import TrustedBy from '../../components/trustedBy/TrustedBy'
-import Slide from '../../components/Slide/Slide'
+import React from "react";
+import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/Slide/Slide";
 import { cards } from "../../data";
-import CatCard from '../../components/catCard/CatCard'
+import CatCard from "../../components/catCard/CatCard";
 
 function Home() {
   return (
@@ -58,8 +58,43 @@ function Home() {
           </div>
         </div>
       </div>
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>fiverr business</h1>
+            <h1>A solution built for business</h1>
+            <p>
+              Upgrade to a curated experience to access vetted talent and
+              exclusive tools
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Talent matching
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Dedicated account management
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Team collaboration tools
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Business payment solutions
+            </div>
+            <button>Explore Fiverr Business</button>
+          </div>
+          <div className="item">
+            <img
+              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624757/business-desktop-870-x1.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
